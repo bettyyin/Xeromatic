59737e1c0f6b20a6a64045442c33287d52c0d2b6
Betty Yin <Betty Yin>
2016-04-09 12:55:03 +1200

    Added a Tweet interface, used getTweets method from api to display recent tweets.

--- Project/Xeromatic/Services/TwitterApiService.cs
+++ Project/Xeromatic/Services/TwitterApiService.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Tweetinvi;
 using Tweetinvi.Core.Credentials;
-using Tweetinvi.Core.Interfaces;
+using Tweet = Xeromatic.Models.Tweet;
 
 namespace Xeromatic.Services
 {
-    public class TwitterApiService
+    public class TwitterApiService : ITwitterService
     {
         // Get keys from: https://apps.twitter.com
         // Wiki for tweetinvi: https://github.com/linvi/tweetinvi/wiki
@@ -16,21 +17,24 @@ namespace Xeromatic.Services
         {
             _creds = new TwitterCredentials
             {
-                ConsumerKey = "Add your ConsumerKey here",
-                ConsumerSecret = "Add your ConsumerSecret here",
-                AccessToken = "Add your AccessToken here",
-                AccessTokenSecret = "Add your AccessTokenSecret here"
+                ConsumerKey = "UCeOk3fECxQ7KBCfXlI5jyAic",
+                ConsumerSecret = "bydz8ebkb8Wr9xOqbJflb6s5QzoxsG69HZE9NBoObDWDzVkZhy",
+                AccessToken = "718575110974603264-6lGrfBafHupcAO9ZCsO0YWg5bT6DqZc",
+                AccessTokenSecret = "JRy4h9UZ1AGgogDdB3u3qbII1oQPaBsLiUqpC4l0N9gES"
             };
         }
 
-        public IEnumerable<ITweet> GetTweets()
+        public IEnumerable<Tweet> GetTweets()
         {
-            var tweets = Auth.ExecuteOperationWithCredentials(_creds, () =>
-            {
-                return Timeline.GetHomeTimeline();
-            });
-
-            return tweets;
+            var tweets = 
+                Auth.ExecuteOperationWithCredentials(_creds, () => Timeline.GetUserTimeline("xero", 10)).ToList();
+            if (tweets.Any())
+                return tweets.Select(t => new Tweet
+                {
+                    Id = t.Id,
+                    Text = t.Text
+                });
+            return new List<Tweet>();
         }
 
     }
